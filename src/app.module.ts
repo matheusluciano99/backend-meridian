32ee985a70c53fdc63c3c460863004d06d2a464e
matheusluciano99 <matheusluciano99@gmail.com>
2025-09-15 12:03:50 -0300

    chore: nest g module ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { PoliciesModule } from './policies/policies.module';
+import { AnchorsModule } from './anchors/anchors.module';
+import { LedgerModule } from './ledger/ledger.module';
+import { SorobanModule } from './soroban/soroban.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule, PoliciesModule, AnchorsModule, LedgerModule, SorobanModule],
   controllers: [AppController],
   providers: [AppService],
 })
